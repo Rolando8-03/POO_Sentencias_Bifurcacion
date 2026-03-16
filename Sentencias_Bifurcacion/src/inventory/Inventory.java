292fb43e0e57e0c2183ff76d47c382dd3e837137
Rolando8 <remena@uamv.edu.ni>
2026-03-16 16:16:22 -0600

    se agrega el codigo de la clase de la contraseña y el inventario

--- Sentencias_Bifurcacion/src/inventory/Inventory.java
+++ Sentencias_Bifurcacion/src/inventory/Inventory.java
@@ -1,4 +1,24 @@
 package inventory;
 
+import javax.swing.*;
+
 public class Inventory {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Buenas tardes...\nTe saluda Logistics\n " +
+                "Sistema para determinar el método de envío de su paquete");
+        double peso = Double.parseDouble(JOptionPane.showInputDialog("Introduce el peso (kg): "));
+        double distancia = Double.parseDouble(JOptionPane.showInputDialog("Introduce la distancia (km): "));
+        String tipo_producto = JOptionPane.showInputDialog(null, "Indica si el producto es frágil (S/N): ");
+        if (peso > 50 && distancia > 100) {
+            JOptionPane.showMessageDialog(null, "El paquete se enviará por transporte pesado");
+        } else if (peso < 10 && peso > 0 && distancia < 20) {
+            JOptionPane.showMessageDialog(null, "El paquete se enviará por dron");
+        } else if (peso < 50 && tipo_producto.equalsIgnoreCase("S")){
+            JOptionPane.showMessageDialog(null, "El paquete se enviará por Envío Premium");
+        } else if (peso <= 0 || distancia <= 0) {
+            JOptionPane.showMessageDialog(null, "El peso o la distancia no puede ser negativo");
+        } else {
+            JOptionPane.showMessageDialog(null, "Su producto no puede ser procesado");
+        }
+    }
 }
--- Sentencias_Bifurcacion/src/password/ValidatePassword.java
+++ Sentencias_Bifurcacion/src/password/ValidatePassword.java
@@ -1,4 +1,34 @@
 package password;
+import javax.swing.*;
+
 
 public class ValidatePassword {
+    public static void main(String[] args) {
+        String password = JOptionPane.showInputDialog("Ingrese la contraseña:");
+
+        int numeros = 0;
+        int mayusculas = 0;
+
+        for (int i = 0; i < password.length(); i++) {
+
+            char c = password.charAt(i);
+
+            if (c >= '0' && c <= '9') {
+                numeros++;
+            }
+
+            if (c >= 'A' && c <= 'Z') {
+                mayusculas++;
+            }
+        }
+        if (password.length() < 8) {
+            JOptionPane.showMessageDialog(null, "Tu contraseña es insegura");
+        } else if (numeros > 0 && mayusculas > 0) {
+            JOptionPane.showMessageDialog(null, "Tu contraseña es fuerte");
+        } else if (numeros > 0) {
+            JOptionPane.showMessageDialog(null, "Tu contraseña es moderada");
+        } else {
+            JOptionPane.showMessageDialog(null, "Tu contraseña es insegura");
+        }
+    }
 }
